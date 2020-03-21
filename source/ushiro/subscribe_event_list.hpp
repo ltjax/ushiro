a5a729498ee3e6198026770ac83b53d5ebd203e9
ltjax <marius.elvert@googlemail.com>
2020-03-21 18:58:31 +0100

    Fix broken nested type specifiers

--- source/ushiro/subscribe_event_list.hpp
+++ source/ushiro/subscribe_event_list.hpp
@@ -13,7 +13,7 @@ template <typename T> struct recursive_event_binder
 
 template <> struct recursive_event_binder<std::tuple<>>
 {
-    template <class Store> static ushiro::event_bus::subscription_list bind(ushiro::event_bus& bus, Store& store)
+    template <class Store> static ushiro::subscription_list bind(ushiro::event_bus& bus, Store& store)
     {
         return {};
     }
@@ -21,7 +21,7 @@ template <> struct recursive_event_binder<std::tuple<>>
 
 template <typename Head, typename... Tail> struct recursive_event_binder<std::tuple<Head, Tail...>>
 {
-    template <class Store> static ushiro::event_bus::subscription_list bind(ushiro::event_bus& bus, Store& store)
+    template <class Store> static ushiro::subscription_list bind(ushiro::event_bus& bus, Store& store)
     {
         auto subscriptions = recursive_event_binder<std::tuple<Tail...>>::bind(bus, store);
         subscriptions += bus.subscribe<Head>(event_applier(store));
@@ -31,7 +31,7 @@ template <typename Head, typename... Tail> struct recursive_event_binder<std::tu
 
 } // namespace detail
 
-template <class Store> ushiro::event_bus::subscription_list subscribe_event_list(ushiro::event_bus& bus, Store& store)
+template <class Store> ushiro::subscription_list subscribe_event_list(ushiro::event_bus& bus, Store& store)
 {
     return detail::recursive_event_binder<typename Store::state_type::event_list>::bind(bus, store);
 }
